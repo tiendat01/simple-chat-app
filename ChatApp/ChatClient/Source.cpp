4c38d6ce4a9c75b08a49c3066934df6e1cef37d9
linhvu2001 <88344263+linhvu2001@users.noreply.github.com>
2022-07-22 09:55:40 +0700

    lcm

--- ChatApp/ChatClient/Source.cpp
+++ ChatApp/ChatClient/Source.cpp
@@ -56,11 +56,22 @@ void showGroupMenu() {
 	printf("1. Create new group chat\n");
 	printf("2. Invite user to your joined group\n");
 	printf("3. Accept/Deny available invitation to join a group\n");
-	printf("4. Leave group chat by group id\n");
-	printf("5. Chat in group by group id\n");
+	printf("4. Chat in group by group id\n");
 
 }
 
+void showChatGroupMenu(){
+	// list of group ...
+
+	// choose group by id ...
+
+	printf("\n\n-------------- CHAT GROUP MENU -----------------\n");
+	printf("Choose option: Press keyboard a number:\n");
+	printf("0. Back\n");
+	printf("1. List online members in group\n");
+	printf("2. Chat in this group\n");
+	printf("3. Leave group chat by group id\n");
+}
 // inbox
 void stopConversationDialog() {
 	printf("\n\n-------------- CONVERSATION BOX -----------------\n");
@@ -70,3 +81,4 @@ void stopConversationDialog() {
 	printf("1. Back to List of your online friends\n");
 	printf("2. Stop (block) the conversation\n");
 }
+
